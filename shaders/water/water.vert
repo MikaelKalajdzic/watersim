f70b436496c82899ff6102409ee486512ce780e2
Thösam <thosamtenzin2002@gmail.com>
2023-05-23 08:55:58 +0200

    deletion of unused parts

--- shaders/water/water.vert
+++
@@ -1,9 +0,0 @@
-// water.vert
-
-precision mediump float;
-
-attribute vec2 position;
-
-void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
-}
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -136,15 +136,6 @@ function applyRipple(x, y, radius, strength) {
     }
 }
 
-// // Set up camera controls
-// const controls = new OrbitControls(camera, renderer.domElement);
-// // controls.enableDamping = true;
-// // controls.dampingFactor = 0.05;
-// controls.screenSpacePanning = false;
-// controls.minDistance = 1;
-// controls.maxDistance = 100;
-// controls.update();
-
 let ripple = false;
 
 // Render loop
